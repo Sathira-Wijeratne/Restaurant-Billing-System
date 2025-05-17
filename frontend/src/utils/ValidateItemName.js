82910b57266515ec8e8ea3118fd8c860231413a0
Sathira-Wijeratne <swije27@gmail.com>
2025-05-17 12:32:00 +0530

    add item price validation method and refactor file from "ValidateItemName" to "ValidateItem"

--- frontend/src/utils/ValidateItemName.js
+++
@@ -1,8 +0,0 @@
-export function validateItemName(itemName){
-    // replace anything that is not an uppercase, lowercase or space with nothing
-    let value = itemName.replace(/[^A-Za-z ]/g, '');
-    // remove one or more white spaces with a single space
-    value = value.replace(/\s+/g, ' ');
-
-    return value;
-}

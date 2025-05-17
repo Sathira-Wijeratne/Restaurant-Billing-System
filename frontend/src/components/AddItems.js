5b1cebc8443ed3aba5d2fac3e268614dbec65088
Sathira-Wijeratne <swije27@gmail.com>
2025-05-17 12:31:04 +0530

    beautify Add item page

--- frontend/src/components/AddItems.js
+++ frontend/src/components/AddItems.js
@@ -1,8 +1,17 @@
-import React, {useEffect, useState} from "react";
+import { useState } from "react";
 import cong from "../Firebase";
-import {collection, addDoc} from "firebase/firestore";
-import {ToastContainer, toast} from "react-toastify"
-import { validateItemName } from "../utils/ValidateItemName";
+import { collection, addDoc } from "firebase/firestore";
+import { ToastContainer, toast } from "react-toastify";
+import { validateItemName, validateItemPrice } from "../utils/ValidateItem";
+import {
+    Button,
+    TextField,
+    Typography,
+    Box,
+    Container,
+    Paper,
+    Grid
+} from "@mui/material";
 
 /**
  * TODO :
@@ -11,7 +20,7 @@ import { validateItemName } from "../utils/ValidateItemName";
  *  handle same item being added more than once
  */
 
-export default function AddItem(){
+export default function AddItem() {
     // variables
     const [itemName, setItemName] = useState("");
     const [itemPrice, setItemPrice] = useState("");
@@ -21,14 +30,14 @@ export default function AddItem(){
     const notifyAddItemFail = () => toast.error("Item not added")
 
     // functions
-    async function saveItem(){
+    async function saveItem() {
         // validate fields
 
         // save the item
         try {
-            const docRef = await addDoc(collection(cong, "items"),{
-                itemName : itemName,
-                itemPrice : itemPrice
+            const docRef = await addDoc(collection(cong, "items"), {
+                itemName: itemName,
+                itemPrice: itemPrice
             });
             console.log("Document written with ID : ", docRef.id);
             notifyAddItem();
@@ -38,44 +47,85 @@ export default function AddItem(){
         }
     }
 
-    function clearFields(){
+    function clearFields() {
         setItemName("");
         setItemPrice("");
     }
 
-    return(
-        <div>
-            <ToastContainer position = "top-center" autoClose = {1000} pauseOnHover = {false} hideProgressBar = {true}/>
-            <h1>Add item form</h1>
-            <div>
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    saveItem();
-                    clearFields();
-                }}>
-                    <div>
-                        <label>Item Name</label>
-                        <input required type="text" value={itemName} placeholder="Enter name" pattern="[A-Za-z ]+" onChange={(e) => {
-                            let value = validateItemName(e.target.value);
-                            // remove leading space if at beginning
-                            if (value.startsWith(' ')) {
-                                value = value.substring(1);
-                            }
-                            setItemName(value);
-                        }}/>
-                    </div>
-                    <div>
-                        <label>Item Price</label>
-                        <input required type="number" value={itemPrice} min={0} placeholder="Enter Price " onChange={(e) => {
-                            setItemPrice(e.target.value);
-                        }}/>
-                    </div>
-                    <button type="submit">Add item</button>
-                </form>
-            </div>
-            <a href="/"><button>Home</button></a>
-            <div>
-            </div>
-        </div>
+    return (
+        <Container maxWidth="md">
+            <ToastContainer
+                position="top-center"
+                autoClose={1000}
+                pauseOnHover={false}
+                hideProgressBar={true}
+                closeButton={false}
+                theme="light"
+            />
+            <Paper elevation={3} sx={{ p: 0, mt: 4 }}>
+    <Typography variant="h5" sx={{ px: 3, py: 2, borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }}>
+        Add New Item
+    </Typography>
+    
+    <form onSubmit={(e) => {
+        e.preventDefault();
+        saveItem();
+        clearFields();
+    }}>
+        <Box sx={{ px: 3, pt: 2, pb: 1 }}>
+            <TextField
+                label="Item Name"
+                fullWidth
+                required
+                value={itemName}
+                margin="normal"
+                placeholder="Enter name"
+                onChange={(e) => {
+                    setItemName(validateItemName(e.target.value));
+                }}
+            />
+            <TextField
+                label="Item Price (Rs.)"
+                type="number"
+                fullWidth
+                required
+                value={itemPrice}
+                placeholder="Enter price"
+                margin="normal"
+                slot={{ min: 0, max: 50000 }}
+                onChange={(e) => {
+                    setItemPrice(validateItemPrice(e.target.value));
+                }}
+            />
+        </Box>
+        
+        <Box sx={{ 
+            display: 'flex', 
+            justifyContent: 'flex-end', 
+            px: 3, 
+            pb: 2,
+            borderTop: '1px solid rgba(0, 0, 0, 0.12)',
+            pt: 2
+        }}>
+            <Button
+                variant="outlined"
+                color="inherit"
+                component="a"
+                href="/"
+                sx={{ mr: 1 }}
+            >
+                Back to Home
+            </Button>
+            <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+            >
+                Add Item
+            </Button>
+        </Box>
+    </form>
+</Paper>
+        </Container>
     );
 }
